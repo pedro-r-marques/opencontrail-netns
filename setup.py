540982a58c139421fdc453e60ade7354192f664b
Édouard Thuleau <edouard.thuleau@cloudwatt.com>
2014-04-17 16:45:55 +0200

    Typo in entry point definition

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setuptools.setup(
 
     entry_points = {
         'console_scripts': [
-            'netns-daemon-start = opencontrail_netns.daemon_start.daemon_start',
-            'netns-daemon-stop = opencontrail_netns.daemon_stop.daemon_stop'
+            'netns-daemon-start = opencontrail_netns.daemon_start:daemon_start',
+            'netns-daemon-stop = opencontrail_netns.daemon_stop:daemon_stop'
         ],
     }
     )
